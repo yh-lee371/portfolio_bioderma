b86aea80328df30ab10280e34e68db08f40b1bf9
yh-lee371 <yh.lee371@gmail.com>
2021-04-15 23:55:19 +0900

    Update version

--- js/index.js
+++ js/index.js
@@ -1,17 +1,54 @@
 $(document).ready(function(){
-  $('#banner').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    autoplay:true,
-    slidesToShow: 1,
-    adaptiveHeight: true
+  $('.gnb > li').mouseover(function(){
+    $('.lnb').removeClass('on');
+    $(this).find('.lnb').addClass('on');
+  })
+
+    $('.gnb').mouseleave(function(){
+    $('.lnb').removeClass('on');
+  })
+
+
+  var slide_01 = new Swiper('.slide_01', {
+      navigation: {
+        nextEl: '.slide01 .next',
+        prevEl: '.slide01 .prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+      },
+      autoplay: {
+        delay: 3000,
+      },
+        parallax: true,
+        speed:2000,
+        // loop: true,
   });
 
-  // a [href] remove
-  $('a[href="#"]').on('click',function(e){
-    e.preventDefault();
+  var slide_02 = new Swiper('.slide_02', {
+      simulateTouch:false,
+      effect:'fade',
   });
+
+  var slide_03 = new Swiper('.slide_03', {
+      parallax: true,
+      speed:2000,
+      simulateTouch:false,
+  });
+
+  slide_01.controller.control = slide_03
+  slide_03.controller.control = slide_02
+
+
+  $('.type_list li').click(function(){
+    idx = $(this).index()+1;
+    $('.type_list li').removeClass('active');
+    $(this).addClass('active');
+    $('.prd_list_wrap .prd_list').removeClass('active');
+    $('#prdList'+idx).addClass('active');
+  })
+
 
   // family-site on/off
   $('.drop_link').on('click', function(){
