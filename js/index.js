156ae08ede8c4a25c7939c13a381a5fc8c967273
yh-lee371 <yh.lee371@gmail.com>
2021-04-21 21:12:42 +0900

    Update version
    - add aos js, favico, meta tag
    - change main_vs img

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  // menu 
   $('.gnb > li').mouseover(function(){
     $('.lnb').removeClass('on');
     $(this).find('.lnb').addClass('on');
@@ -8,7 +9,7 @@ $(document).ready(function(){
     $('.lnb').removeClass('on');
   })
 
-
+  //swiper
   var slide_01 = new Swiper('.slide_01', {
       navigation: {
         nextEl: '.slide01 .next',
@@ -19,28 +20,37 @@ $(document).ready(function(){
         type: 'fraction',
       },
       autoplay: {
-        delay: 3000,
+        delay: 2500,
       },
-        parallax: true,
-        speed:2000,
-        // loop: true,
+      parallax: true,
+      speed:2000,
+      loop: true,
   });
 
   var slide_02 = new Swiper('.slide_02', {
       simulateTouch:false,
       effect:'fade',
+      loop: true,
   });
 
   var slide_03 = new Swiper('.slide_03', {
       parallax: true,
       speed:2000,
       simulateTouch:false,
+      loop: true,
   });
 
   slide_01.controller.control = slide_03
   slide_03.controller.control = slide_02
 
+  //aos 
+  AOS.init() ; 
+  AOS.init({
+    duration: 1000
+  })
 
+
+  // type_list 
   $('.type_list li').click(function(){
     idx = $(this).index()+1;
     $('.type_list li').removeClass('active');
